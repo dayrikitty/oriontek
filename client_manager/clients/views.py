fd48845aba3b7778d4488406c3db5aa67d5d0201
Dayrimell Tejada <dayritej@gmail.com>
2024-12-18 23:47:36 -0400

    add views routes auth and apply migrations

--- client_manager/clients/views.py
+++ client_manager/clients/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Client, Address
+from .serializers import ClientSerializer, AddressSerializer
 
-# Create your views here.
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
